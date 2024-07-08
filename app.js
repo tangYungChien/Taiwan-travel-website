47385dc15bc90f2f1046f3cea05029f80f11f3b9
Maggie <maggie10039@gmail.com>
2024-07-08 16:14:18 +0800

    v3

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function addLike(e, spotID) {
   btnDiv.appendChild(newButton);
   newButton.appendChild(newItag);
 
-  // //收藏景點機加入超連結，連結到上面的景點說明
+  // //收藏景點加入超連結，連結到上面的景點說明
   if (likeOne.classList.contains("PalaceMuseum")) {
     CardImgWord.innerHTML = "國立故宮博物院";
     CardImgWord.setAttribute("href", "#PalaceMuseum");
@@ -89,17 +89,13 @@ function addLike(e, spotID) {
   }
   //監聽a移到螢幕中間
   let allLinks = document.querySelectorAll(".CardImgDiv a");
-  console.log(allLinks);
   allLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetId = link.getAttribute("href");
+      e.preventDefault(); //阻止跳轉到 href 屬性指定的位置
+      const targetId = link.getAttribute("href"); //獲取鏈接的 href 屬性值，這是目標元素的 ID
       if (targetId) {
-        const targetElement = document.querySelector(targetId);
-
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
-        }
+        const targetElement = document.querySelector(targetId); // 根據 ID 查找目標元素
+        targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
       }
     });
   });
@@ -107,17 +103,16 @@ function addLike(e, spotID) {
   //垃圾桶功能
   let allTrash = document.querySelectorAll(".trash-button");
   let button2 = document.getElementById(spotID);
-  console.log(allTrash);
 
   allTrash.forEach((trash) => {
     trash.addEventListener("click", () => {
-      console.log(trash.parentElement.parentElement);
-      trash.parentElement.parentElement.classList.add("remove");
+      trash.parentElement.parentElement.classList.add("remove"); //移除動畫
       button2.innerHTML = "+ 加入收藏";
     });
   });
   allTrash.forEach((trash) => {
     let form = trash.parentElement.parentElement;
+    //等動畫結束才做刪除
     form.addEventListener("transitionend", (e) => {
       e.target.remove();
       checkElementExistence();
@@ -125,12 +120,10 @@ function addLike(e, spotID) {
   });
 }
 checkElementExistence();
-//無收藏景點
+//無收藏景點開關
 function checkElementExistence() {
-  // console.log("經過checkElementExistence");
   let likeOneDiv = document.getElementsByClassName("likeOne")[0]; // 获取第一个具有类名 'likeOne' 的元素
   let noAttractionsMessage = document.getElementById("noAttractionsMessage");
-  // console.log(!likeOneDiv);
   if (!likeOneDiv) {
     noAttractionsMessage.style.display = "block";
   } else {
@@ -139,21 +132,21 @@ function checkElementExistence() {
 }
 
 // 讓atag在畫面中間
-function aTag() {
-  let allLinks = document.querySelectorAll("CardImgDiv.a");
-  allLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetId = link.getAttribute("href");
-      if (targetId) {
-        const targetElement = document.querySelector(targetId);
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
-        }
-      }
-    });
-  });
-}
+// function aTag() {
+//   let allLinks = document.querySelectorAll("CardImgDiv.a");
+//   allLinks.forEach((link) => {
+//     link.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       const targetId = link.getAttribute("href");
+//       if (targetId) {
+//         const targetElement = document.querySelector(targetId);
+//         if (targetElement) {
+//           targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
+//         }
+//       }
+//     });
+//   });
+// }
 
 fetch(
   "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-1DDD4E0F-7770-4EF4-9ACE-009BDB05AEA3&StationName=%E8%87%BA%E5%8C%97,%E8%87%BA%E4%B8%AD,%E9%AB%98%E9%9B%84,%E8%8A%B1%E8%93%AE"
@@ -215,6 +208,7 @@ fetch(
     weatherStatus = document.querySelector(".weatherStatus");
   });
 
+//天氣處理
 fetch(
   "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWA-1DDD4E0F-7770-4EF4-9ACE-009BDB05AEA3&locationName=%E8%8A%B1%E8%93%AE%E7%B8%A3,%E8%87%BA%E5%8C%97%E5%B8%82,%E8%87%BA%E4%B8%AD%E5%B8%82,%E9%AB%98%E9%9B%84%E5%B8%82&elementName=WeatherDescription&sort=time"
 )
@@ -233,7 +227,7 @@ fetch(
       weatherWeek = data.records.locations[0].location[2];
     }
 
-    // 創建一個空陣列來存儲我們選取的資料
+    // 創建一個空陣列來存儲選取的資料
     const selectedData = [];
 
     // 選取每個startTime=06:00:00的資料
@@ -248,17 +242,17 @@ fetch(
     selectedData.forEach((data) => {
       // 從startTime中提取日期
       const date = new Date(data.startTime.split(" ")[0]);
-      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const month = (date.getMonth() + 1).toString().padStart(2, "0"); //padStart(2, "0") 方法確保字符串至少有兩個字符，否則補0
       const day = date.getDate().toString().padStart(2, "0");
 
       // 取得星期幾的簡寫
       const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-      const dayOfWeek = days[date.getDay()];
+      const dayOfWeek = days[date.getDay()]; //返回星期幾，範圍是 0-6
       // 從"value"中擷取天氣狀態
       const weatherStatus = data.elementValue[0].value.split("。")[0];
 
       // 從"value"中擷取溫度範圍
-      const valueParts = data.elementValue[0].value.split("。");
+      const valueParts = data.elementValue[0].value.split("。"); //切成list
       const temperaturePartIndex = valueParts.findIndex((part) =>
         part.includes("溫度攝氏")
       );
@@ -266,7 +260,7 @@ fetch(
         temperaturePartIndex !== -1 ? valueParts[temperaturePartIndex] : "";
       const temperatureRange = temperaturePart
         ? temperaturePart.split("至")
-        : [];
+        : []; //使用 split("至") 將句子按 "至" 分割成兩部分，得到最低溫和最高溫。
 
       // 分別取得最低溫和最高溫
       const minTemperature =
@@ -302,26 +296,28 @@ fetch(
   </div>
         `;
     });
-    processWeatherData(
-      document.querySelectorAll(".Weekweather"),
-      document.querySelector(".weatherStatus")
-    );
+    setTimeout(() => {
+      processWeatherData(
+        document.querySelectorAll(".Weekweather"),
+        document.querySelector(".weatherStatus")
+      );
+    }, 100); // 可以根據需要調整延遲時間
   });
 
 // 判斷是否需要顯示雨天景點
 function processWeatherData(Weekweather, weatherStatus) {
   let rainForecast = false;
-  // console.log(weatherStatus);
   for (let day of Weekweather) {
     const imgSrc = day.querySelector("img").getAttribute("src");
     if (imgSrc === "images/rainy.png") {
       rainForecast = true;
-      break;
+      break; //若week中有照片是雨天則顯示雨天景點
     }
   }
   const spot = document.querySelector(".spot");
   const txt = weatherStatus.textContent;
 
+  //若week中有照片是雨天或是今日天氣狀態有雨之關鍵字
   if (txt.includes("雨") || rainForecast) {
     spot.style.display = "block";
   } else {
